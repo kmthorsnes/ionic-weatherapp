efe4b7f1553a79733e6e57742dbeb8e6dae9b9a0
kalle <kmthorsnes@gmail.com>
2018-04-27 18:21:39 +0200

    to start with home.ts

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -4,9 +4,16 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class WeatherProvider {
-  apiKey = ''
+  apiKey = '6c2619d9822ad1d2';
+  url;
+
   constructor(public http: HttpClient) {
     console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
   }
 
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+      .map(res => res.json());
+  }
 }
